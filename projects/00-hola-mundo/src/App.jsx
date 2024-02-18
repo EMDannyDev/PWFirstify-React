e8b2cb0970586b18422cb0f7b282c5c0270d2c2b
MendozaEmerson <emendozahi@unsa.edu.pe>
2024-02-18 09:35:59 -0500

    Se agrego estilos y componentes

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+export function App() {
+    return (
+        <div>
+            <article>
+                <header>
+                    <img alt="Avatar de Usuario" src="https://unavatar.io/rosaria" />
+                    <div>
+                        <strong>Leviathan</strong>
+                        <span>@levi</span>
+                    </div>
+                </header>
+
+                <aside>
+                    <button>
+                        Seguir
+                    </button>
+                </aside>
+            </article>
+        </div>
+    )
+}
