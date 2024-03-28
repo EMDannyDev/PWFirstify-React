56647fa1f8dbd7ca3578f7333542c9d0ea164154
MendozaEmerson <emendozahi@unsa.edu.pe>
2024-03-28 14:27:56 -0500

    Se le agrego la funcion de cmabiar de turno al seleccionar casillas, asi como tambien visualizar dicho simbolo en la casilla.

--- projects/02-tic-tac-toc/src/App.jsx
+++ projects/02-tic-tac-toc/src/App.jsx
@@ -10,16 +10,30 @@ const TURNS = {
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div className={className}>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.x)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn == TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
+  }
 
   return (
     <main className='board'>
@@ -31,6 +45,7 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
                 {board[index]}
               </Square>
