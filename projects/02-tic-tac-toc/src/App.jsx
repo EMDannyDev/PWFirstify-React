e926949b981357bab0112d8977c88b91b317a203
MendozaEmerson <emendozahi@unsa.edu.pe>
2024-03-28 11:57:22 -0500

    Se coloco una tabla para separar a sus valores de indice y que son tomados como children.

--- projects/02-tic-tac-toc/src/App.jsx
+++ projects/02-tic-tac-toc/src/App.jsx
@@ -8,6 +8,13 @@ const TURNS = {
 
 const board = Array(9).fill(null)
 
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
 function App() {
   return (
     <main className='board'>
@@ -16,11 +23,12 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <div className='cell' key={index}>
-                <span className='cell__content'>
-                  {index}
-                </span>
-              </div>
+              <Square
+                key={index}
+                index={index}
+              >
+                {index}
+              </Square>
             )
           })
         }
